79cf401d54462ea8734dd5b643e05535c63dc544
vkirillov <vkirillov@fil-it.ru>
2019-12-21 21:43:52 +0300

    add light

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include <stdlib.h>
-#include <math.h>
+#include <cstdlib>
+#include <cmath>
 #include <GL/glut.h>
 #include "includes/SOIL.h"
 
@@ -13,11 +13,7 @@ GLfloat sphereTransX;
 GLfloat sphereTransY;
 GLfloat sphereTransZ;
 
-GLfloat light1TransX;
-GLfloat light1TransY;
-GLfloat light1TransZ;
-
-GLfloat LightPosition1[]= { light1TransX, light1TransY, light1TransZ, 1.0f };     // Позиция света
+GLfloat LightPosition1[] = {sphereTransX, sphereTransY, sphereTransZ, 1.0f};     // Позиция света
 
 void init(void) {
     //загрузка текстур с помощью библиотеки SOIL
@@ -38,11 +34,10 @@ void init(void) {
     glShadeModel(GL_SMOOTH);      // Разрешение сглаженного закрашивания
     glClearColor(0.0f, 0.0f, 0.0f, 0.5f); // Черный фон
     glClearDepth(1.0f);           // Установка буфера глубины
-    glEnable(GL_DEPTH_TEST);      // Разрешить тест глубины
     glDepthFunc(GL_LEQUAL);       // Тип теста глубины
 
-    GLfloat LightAmbient1[]= { 0.5f, 0.5f, 0.5f, 1.0f }; // Значения фонового света
-    GLfloat LightDiffuse1[]= { 1.0f, 1.0f, 1.0f, 1.0f }; // Значения диффузного света
+    GLfloat LightAmbient1[] = {0.5f, 0.5f, 0.5f, 1.0f}; // Значения фонового света
+    GLfloat LightDiffuse1[] = {1.0f, 1.0f, 1.0f, 1.0f}; // Значения диффузного света
     glLightfv(GL_LIGHT1, GL_AMBIENT, LightAmbient1);    // Установка Фонового Света
     glLightfv(GL_LIGHT1, GL_DIFFUSE, LightDiffuse1);    // Установка Диффузного Света
     glLightfv(GL_LIGHT1, GL_POSITION, LightPosition1);   // Позиция света
@@ -51,7 +46,9 @@ void init(void) {
     glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
+    glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CCW);
     glCullFace(GL_BACK);
 }
 
@@ -66,34 +63,50 @@ void drawPyramid() {
     glRotatef(rtri, 0.0f, 1.0f, 0.0f);                 // Вращение пирамиды по оси Y
 
     glBegin(GL_TRIANGLES);                          // Начало рисования пирамиды
-    glNormal3f( 0.0f, 0.0f, 1.0f);     // Нормаль указывает на наблюдателя
-    glTexCoord2f(0.5f, 1.0f); glVertex3f(0.0f, 1.0f, 0.0f);                  // Верх треугольника (Передняя)
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);                  // Левая точка
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);                  // Правая точка
+    glNormal3f(0.0f, 0.0f, 1.0f);     // Нормаль указывает на наблюдателя
+    glTexCoord2f(0.5f, 1.0f);
+    glVertex3f(0.0f, 1.0f, 0.0f);                  // Верх треугольника (Передняя)
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);                  // Левая точка
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);                  // Правая точка
 
-    glNormal3f( 1.0f, 0.0f, 0.0f);     // Нормаль указывает вправо
-    glTexCoord2f(0.5f, 1.0f); glVertex3f(0.0f, 1.0f, 0.0f);                  // Верх треугольника (Правая)
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);                  // Лево треугольника (Правая)
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);                 // Право треугольника (Правая)
+    glNormal3f(1.0f, 0.0f, 0.0f);     // Нормаль указывает вправо
+    glTexCoord2f(0.5f, 1.0f);
+    glVertex3f(0.0f, 1.0f, 0.0f);                  // Верх треугольника (Правая)
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);                  // Лево треугольника (Правая)
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);                 // Право треугольника (Правая)
 
-    glNormal3f( 0.0f, 0.0f,-1.0f);     // Нормаль указывает от наблюдателя
-    glTexCoord2f(0.5f, 1.0f); glVertex3f(0.0f, 1.0f, 0.0f);                  // Низ треугольника (Сзади)
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);                 // Лево треугольника (Сзади)
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);                 // Право треугольника (Сзади)
+    glNormal3f(0.0f, 0.0f, -1.0f);     // Нормаль указывает от наблюдателя
+    glTexCoord2f(0.5f, 1.0f);
+    glVertex3f(0.0f, 1.0f, 0.0f);                  // Низ треугольника (Сзади)
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);                 // Лево треугольника (Сзади)
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);                 // Право треугольника (Сзади)
 
     glNormal3f(-1.0f, 0.0f, 0.0f);     // Нормаль указывает влево
-    glTexCoord2f(0.5f, 1.0f); glVertex3f(0.0f, 1.0f, 0.0f);                  // Верх треугольника (Лево)
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);                  // Лево треугольника (Лево)
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);                  // Право треугольника (Лево)
+    glTexCoord2f(0.5f, 1.0f);
+    glVertex3f(0.0f, 1.0f, 0.0f);                  // Верх треугольника (Лево)
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);                  // Лево треугольника (Лево)
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);                  // Право треугольника (Лево)
     glEnd();                                                // Кончили рисовать пирамиду
 
 
     glBegin(GL_POLYGON);                          // Начало рисования основания пирамиды
-    glNormal3f( 0.0f,-1.0f, 0.0f);     // Нормаль указывает вниз
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Верх право
-    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Верх лево
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Низ лево
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Низ право
+    glNormal3f(0.0f, -1.0f, 0.0f);     // Нормаль указывает вниз
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);    // Верх право
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);    // Верх лево
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);    // Низ лево
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);    // Низ право
     glEnd();                                                // Кончили рисовать основание пирамиды
 
     glFlush();
@@ -109,51 +122,75 @@ void drawCube() {
 
     //Рислвание куба
     glLoadIdentity();
-    glTranslatef(3.0f,-2.0f,-15.0f);          // Сдвинуть вправо и вглубь экрана
-    glRotatef(rquad,1.0f,1.0f,1.0f);        // Вращение куба по X, Y & Z
+    glTranslatef(3.0f, -2.0f, -15.0f);          // Сдвинуть вправо и вглубь экрана
+    glRotatef(rquad, 1.0f, 1.0f, 1.0f);        // Вращение куба по X, Y & Z
 
     glBegin(GL_QUADS);
     // Передняя грань
-    glNormal3f( 0.0f, 0.0f, 1.0f);     // Нормаль указывает на наблюдателя
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Низ лево
-    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Низ право
-    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Верх право
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Верх лево
+    glNormal3f(0.0f, 0.0f, 1.0f);     // Нормаль указывает на наблюдателя
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);    // Низ лево
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);    // Низ право
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);    // Верх право
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);    // Верх лево
 
     // Задняя грань
-    glNormal3f( 0.0f, 0.0f,-1.0f);     // Нормаль указывает от наблюдателя
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Низ право
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Верх право
-    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Верх лево
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Низ лево
+    glNormal3f(0.0f, 0.0f, -1.0f);     // Нормаль указывает от наблюдателя
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);    // Низ право
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);    // Верх право
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);    // Верх лево
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);    // Низ лево
 
     // Верхняя грань
-    glNormal3f( 0.0f, 1.0f, 0.0f);     // Нормаль указывает вверх
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Верх лево
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Низ лево
-    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Низ право
-    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Верх право
+    glNormal3f(0.0f, 1.0f, 0.0f);     // Нормаль указывает вверх
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);    // Верх лево
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);    // Низ лево
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);    // Низ право
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);    // Верх право
 
     // Нижняя грань
-    glNormal3f( 0.0f,-1.0f, 0.0f);     // Нормаль указывает вниз
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Верх право
-    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Верх лево
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Низ лево
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Низ право
+    glNormal3f(0.0f, -1.0f, 0.0f);     // Нормаль указывает вниз
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);    // Верх право
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);    // Верх лево
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);    // Низ лево
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);    // Низ право
 
     // Правая грань
-    glNormal3f( 1.0f, 0.0f, 0.0f);     // Нормаль указывает вправо
-    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Низ право
-    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Верх право
-    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Верх лево
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Низ лево
+    glNormal3f(1.0f, 0.0f, 0.0f);     // Нормаль указывает вправо
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);    // Низ право
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);    // Верх право
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);    // Верх лево
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);    // Низ лево
 
     // Левая грань
     glNormal3f(-1.0f, 0.0f, 0.0f);     // Нормаль указывает влево
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Низ лево
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Низ право
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Верх право
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Верх лево
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);    // Низ лево
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);    // Низ право
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);    // Верх право
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);    // Верх лево
 
     glEnd();                             // Закончили квадраты
 
@@ -169,17 +206,17 @@ void drawLightSource() {
     glTranslatef(sphereTransX, sphereTransY, sphereTransZ);
     glColor3f(1.0, 1.0, 1.0);
     glScalef(1.0, 1.0, 1.0);
-    glutSolidSphere(0.5, 20, 20);
+    glutSolidSphere(0.2, 20, 20);
     glFlush();
 
-    sphereTransX = 9.0f * sin(glutGet(GLUT_ELAPSED_TIME) * 0.001);
+    sphereTransX = 7.0f * sin(glutGet(GLUT_ELAPSED_TIME) * 0.001);
     sphereTransY = 1.0f * cos(glutGet(GLUT_ELAPSED_TIME) * 0.001);
     sphereTransZ = -15.0 + 10.0f * cos(glutGet(GLUT_ELAPSED_TIME) * 0.001);
 }
 
 void drawSphere() {
     glLoadIdentity();
-    glTranslatef(0.0f,3.0f,-15.0f);          // Сдвинуть вправо и вглубь экрана
+    glTranslatef(0.0f, 3.0f, -15.0f);          // Сдвинуть вправо и вглубь экрана
     glColor3f(1.0, 1.0, 1.0);
     glScalef(1.0, 1.0, 1.0);
     glutSolidSphere(1.0, 20, 20);
@@ -187,10 +224,6 @@ void drawSphere() {
 }
 
 void drawLight() {
-    light1TransY = 10.0f * sin(glutGet(GLUT_ELAPSED_TIME) * 0.001);
-    light1TransY = 2.0f * cos(glutGet(GLUT_ELAPSED_TIME) * 0.001);
-    light1TransZ = -14.0 + 10.0f * cos(glutGet(GLUT_ELAPSED_TIME) * 0.001);
-
     glLightfv(GL_LIGHT1, GL_POSITION, LightPosition1);   // Позиция света
 }
 
@@ -223,10 +256,6 @@ void reshape(int w, int h) {
     glLoadIdentity();//загружаем в нее единичную матрицу
 
     gluPerspective(45.0f, (GLfloat) w / (GLfloat) h, 0.1f, 100.0f);//задаем перспективное преобразование
-    //задаем параметры камеры
-//    gluLookAt(1.0, -1.0, 8.0, //откуда смотрим
-//              0.0, 0.0, 0.0,  //куда смотрим
-//              2.0, 8.0, 1.0); //направление вверх
 
     glMatrixMode(GL_MODELVIEW);    //задаем модельно-видовую матрицу
 
